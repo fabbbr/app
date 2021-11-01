3748da86bd50acfb19dc838af1826655c2259b54
Lahaxe Hugo <lahaxeh@gmail.com>
2021-11-01 15:31:01 +0100

    utils localStorage

---
+++ utils/localStorage.js
@@ -0,0 +1,18 @@
+import * as SecureStore from 'expo-secure-store'
+
+export async function save(key, value) {
+    return await SecureStore.setItemAsync(key, value)
+}
+
+export async function get(key) {
+    let result = await SecureStore.getItemAsync(key)
+    if(result) {
+        return result
+    } else {
+        return null
+    }
+}
+
+export async function remove(key) {
+    return await SecureStore.deleteItemAsync(key)
+}
