bad9faec8dae4d7a3ccedabb09c1827b37b284ea
Lahaxe Hugo <lahaxeh@gmail.com>
2021-11-01 15:32:13 +0100

    setup safearea and test

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View, SafeAreaView, StyleSheet, ScrollView } from 'react-native'
 import BottomNavigation from './components/BottomNavigation'
 import pagesDictionnary from './dictionaries/pagesDictionnary'
+import * as localStorage from './utils/localStorage'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   const [page, navigate] = useState(pagesDictionnary.HOME)
+  const [init, setInit] = useState(true)
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    localStorage.get('uToken').then(token => {
+      if(token !== null) {
+        
+      }
+      setUser(token)
+    })
+  }, [])
 
   return (
-    <View style={styles.container}>
-      <page.Component />   
+    <SafeAreaProvider>
+    <SafeAreaView style={styles.container}  >
+      <ScrollView>
+        <View style={styles.pageView}>
+          <page.Component />   
+        </View>
+      </ScrollView>
       <BottomNavigation navigate={navigate} />
-    </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -18,8 +37,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
     fontFamily: 'Roboto'
   },
+  pageView: {
+    padding: 10
+  }
 });
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -4,7 +4,46 @@ import { StyleSheet, View, Text } from 'react-native'
 export default function HomePage() {
     return (
         <View style={styles.container}>
-            <Text>Page 1</Text>
+            <Text>
+                Page 1
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
         </View>
     )
 }
