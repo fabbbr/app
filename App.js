1f946c352547c63b97c1649cfa41c7308aaa7bae
Lahaxe Hugo <lahaxeh@gmail.com>
2021-11-10 22:10:15 +0100

    testing new navigation

--- App.js
+++ App.js
@@ -1,46 +1,89 @@
 import React, { useEffect, useState } from 'react'
-import { View, SafeAreaView, StyleSheet, ScrollView } from 'react-native'
-import BottomNavigation from './components/BottomNavigation'
-import pagesDictionnary from './dictionaries/pagesDictionnary'
-import * as localStorage from './utils/localStorage'
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { View, StyleSheet, Text, Button, TextInput } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+function HomeScreen({ navigation, route }) {
+    React.useEffect(() => {
+        if (route.params?.post) {
+            // Post updated, do something with `route.params.post`
+            // For example, send the post to the server
+        }
+    }, [route.params?.post]);
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            {/* <Button
+                title="Create post"
+                onPress={() => navigation.navigate('CreatePost')}
+            /> */}
+            <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+        </View>
+    );
+}
+
+function 
+
+// function CreatePostScreen({ navigation, route }) {
+//     const [postText, setPostText] = React.useState('');
+
+//     return (
+//         <>
+//             <TextInput
+//                 multiline
+//                 placeholder="What's on your mind?"
+//                 style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+//                 value={postText}
+//                 onChangeText={setPostText}
+//             />
+//             <Button
+//                 title="Done"
+//                 onPress={() => {
+//                     // Pass and merge params back to home screen
+//                     navigation.navigate({
+//                         name: 'Home',
+//                         params: { post: postText },
+//                         merge: true,
+//                     });
+//                 }}
+//             />
+//         </>
+//     );
+// }
+
+function StackScreen() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{ title: 'My home' }}
+            />
+            <Stack.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={({ route }) => ({ title: route.params.name })}
+            />
+        </Stack.Navigator>
+    );
+}
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [page, navigate] = useState(pagesDictionnary.HOME)
-  const [init, setInit] = useState(true)
-  const [user, setUser] = useState()
-
-  useEffect(() => {
-    localStorage.get('uToken').then(token => {
-      if(token !== null) {
-        
-      }
-      setUser(token)
-    })
-  }, [])
-
-  return (
-    <SafeAreaProvider>
-    <SafeAreaView style={styles.container}  >
-      <ScrollView>
-        <View style={styles.pageView}>
-          <page.Component />   
-        </View>
-      </ScrollView>
-      <BottomNavigation navigate={navigate} />
-    </SafeAreaView>
-    </SafeAreaProvider>
-  );
+    return (
+        <NavigationContainer>
+            <StackScreen />
+        </NavigationContainer>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    fontFamily: 'Roboto'
-  },
-  pageView: {
-    padding: 10
-  }
+    container: {
+        backgroundColor: '#FFF',
+        padding: 10,
+        alignItems: 'center'
+    }
 });
