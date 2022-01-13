f841af202dfecb2a12e38c7107ebcd296b028e26
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-13 09:27:07 +0100

    feat: adding dash option to apptitle

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -3,23 +3,31 @@ import { StyleSheet, Text, View } from 'react-native'
 import GlobalStyle from '@styles/GlobalStyle'
 import Icon3lines from '@icons/title-3lines.svg'
 
-export default function AppTitle({text, align, icon}) {
+export default function AppTitle({text, align, icon, dash}) {
     if(align === undefined || !align.length) align = 'flex-start'
+
+    let Dash
+    if(dash === true) {
+        Dash = <View style={styles.line}></View>
+    }
 
     let Icon
     switch(icon) {
         case '3lines': 
-            Icon = <Icon3lines />
+            Icon = (
+                <View style={styles.icon_box}>
+                    <Icon3lines />
+                </View>
+            )
             break
     }
     
 
     return (
         <View style={{...styles.box, justifyContent: align}}>
+            {Dash}
             <Text style={{...styles.text}}>{text}</Text>
-            <View style={styles.icon_box}>
-                {Icon}
-            </View>
+            {Icon}
         </View>
     )
 }
@@ -32,9 +40,16 @@ const styles = StyleSheet.create({
         color: GlobalStyle.color.primary,
         fontWeight: 'bold',
         fontSize: GlobalStyle.size.title,
-        marginBottom: 10
+        marginBottom: 5
     },
     icon_box: {
         marginLeft: 2
+    },
+    line: {
+        marginTop: 14,
+        marginRight: 6,
+        width: 16,
+        height: 1,
+        backgroundColor: GlobalStyle.color.primary,
     }
 })
