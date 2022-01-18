75ef05f435529d3101e6a51f012ddb71cd1287a0
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-18 17:26:32 +0100

    refactor: app async loading for user + adding async fonts loading

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,35 @@
 import  React, { useEffect, useState } from 'react'
-import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useDispatch, useSelector } from 'react-redux'
+import Apploading from 'expo-app-loading'
 
 import BottomNavigation from '@components/BottomNavigation'
 import { setUserInit } from '@slices/auth'
+import { getFonts } from '@constants/fonts'
 
 export default function App() {
-    const [loading, setLoading] = useState(true)
-    const dispatch = useDispatch()
-    const { init } = useSelector((state) => state.auth)
+    const [loaded, setLoaded] = useState(false)
 
-    useEffect(() => {
-        if(init) {
-            dispatch(setUserInit())
-                .unwrap()
-                .then(() => {
-                    setLoading(false)
-                })
-        }
-    }, [])
+    const dispatch = useDispatch()
+    const { authInit } = useSelector((state) => state.auth)
+
+    const asyncStart = async () => {
+        if(authInit) await dispatch(setUserInit())
+        getFonts()
+    }
 
     return (
         <SafeAreaView style={{flex: 1}}>
-            {loading ?
-                (<Text>Loading</Text>)
-                :
+            {!loaded ?
+                 <Apploading
+                 startAsync={asyncStart}
+                 onFinish={() => {
+                   setLoaded(true)
+                 }}
+                 onError={console.warn}
+               />
+            :
                 (<NavigationContainer>
                     <BottomNavigation />
                 </NavigationContainer>)
