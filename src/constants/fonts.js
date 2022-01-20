2c4b065889b4dcab72632802d3042bcb36c72ac7
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-20 12:50:58 +0100

    feat: app style define h1, h2, h3, text

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -3,6 +3,7 @@ import * as Font from 'expo-font'
 export const getFonts = () => {
     return Font.loadAsync({
       RecoletaRegular: require("@fonts/Recoleta/Recoleta-Regular.ttf"),
+      RecoletaMedium: require("@fonts/Recoleta/Recoleta-Medium.ttf"),
       RecoletaBold: require("@fonts/Recoleta/Recoleta-Bold.ttf"),
       RiposteRegular: require("@fonts/Riposte/Riposte-Regular.otf")
     })
