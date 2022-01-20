58f63add6336f990c36b140ea3b0b4607458ada3
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-20 12:55:15 +0100

    style: product miniature finished

--- src/components/ProductMiniature.js
+++ src/components/ProductMiniature.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
-import GlobalStyle from '@styles/GlobalStyle'
+
 import Flag from '@components/Flag'
 import YellowStar from '@icons/yellow-star.svg'
+import GlobalStyle from '@styles/GlobalStyle'
+import AppStyle from '@styles/AppStyle'
+
 
 export default function ProductMiniature({ product }) {
     return (
         <View style={styles.container}>
             <View style={styles.img}></View>
             <View style={styles.container2}>
-                <Text style={styles.title}>{product.name}</Text>
+                <Text style={AppStyle.h3}>{product.name}</Text>
                 
                 <View style={styles.container3}>
                     <Flag code={product.country} />
                     <View style={styles.vertical_lign}></View>
-                    <Text style={styles.seller_name}>{product.seller_name}</Text>
+                    <Text style={AppStyle.text}>{product.seller_name}</Text>
                 </View>
                 
                 <View style={styles.horizontal_lign}></View>
 
                 <View style={styles.bottom}>
                     <View style={styles.bottom_left}>
-                        <YellowStar style={styles.star} />
+                        <YellowStar />
                         <Text style={styles.rating}>{product.rating}</Text>
                         <Text style={styles.review}>({product.review})</Text>
                     </View>
@@ -40,32 +43,27 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 1,
         borderColor: GlobalStyle.color.lightgray,
+        overflow: 'hidden',
         width: 240
     },
     container2: {
-        padding: 10
+        padding: 10,
+        paddingTop: 15
     },
     img: {
-        backgroundColor: 'red',
-        height: 240,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10
-    },
-    title: {
-        fontFamily: 'Recoleta'
+        backgroundColor: 'lightgray',
+        height: 240
     },
     container3: {
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: 5
     },
     vertical_lign: {
         backgroundColor: GlobalStyle.color.lightgray,
         width: 1,
         height: 20,
         marginHorizontal: 10
-    },
-    seller_name: {
-        color: GlobalStyle.color.text
     },
     horizontal_lign: {
         backgroundColor: GlobalStyle.color.lightgray,
@@ -79,22 +77,21 @@ const styles = StyleSheet.create({
     },
     bottom_left: {
         flexDirection: 'row',
-        padding: 5
-    },
-    star: {
-        marginTop: 1
+        padding: 5,
+        marginTop: 2
     },
     rating: {
-        marginLeft: 5,
-        color: GlobalStyle.color.text
+        ...AppStyle.text,
+        marginLeft: 5
     },
     review: {
+        ...AppStyle.text,
         marginLeft: 5,
         color: GlobalStyle.color.lightgray2
     },
     price: {
+        ...AppStyle.h3_bold,
         color: GlobalStyle.color.primary,
-        fontWeight: 'bold',
         backgroundColor: GlobalStyle.color.background,
         paddingVertical: 5,
         paddingHorizontal: 10,
