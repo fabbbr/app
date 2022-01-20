3e063b19c26865222b960be58735f6cd1b1d590c
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-20 12:59:57 +0100

    remove console log

--- src/containers/ProductsSlider.js
+++ src/containers/ProductsSlider.js
@@ -3,7 +3,6 @@ import { ScrollView, Text } from 'react-native'
 import ProductMiniature from '@components/ProductMiniature'
 
 export default function ProductsSlider({ products }) {
-    console.log(products)
     const items = products.map((item, i) => <ProductMiniature product={item} key={i} />)
 
     return (
