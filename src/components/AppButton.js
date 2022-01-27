e97b28604bbda5885f4b68c01cb9096326603d19
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-27 23:15:46 +0100

    feat: adding loading option to AppButton

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -1,29 +1,33 @@
 import React from 'react'
-import { TouchableOpacity, Text, View } from 'react-native'
+import { TouchableOpacity, Text, View, ActivityIndicator } from 'react-native'
 import ButtonStyle from '@styles/ButtonStyle'
 import IconGoogle from '@icons/google.svg'
 
-
-export default function AppButton({ type, text, onPress }) {
-    if(type === undefined || !type.length) type = 'default'
-    const type_text = type+'_text'
+export default function AppButton({ type, text, onPress, loading }) {
+    if (type === undefined || !type.length) type = 'default'
+    const type_text = type + '_text'
 
     let Icon
-    switch(type) {
-        case 'google': 
-            Icon = <IconGoogle style={{marginRight: 10}} />
+    switch (type) {
+        case 'google':
+            Icon = <IconGoogle style={{ marginRight: 10 }} />
             break
     }
 
-    return(
-        <TouchableOpacity 
-            onPress={onPress} 
+    return (
+        <TouchableOpacity
+            onPress={onPress}
             style={ButtonStyle[type]}
-            activeOpacity={.8}
+            activeOpacity={0.8}
         >
-            <View>
-                {Icon}
-            </View>
+            {loading ? (
+                <ActivityIndicator
+                    size='small'
+                    color='#ddd'
+                    style={{ marginRight: 10 }}
+                />
+            ) : null}
+            <View>{Icon}</View>
             <Text style={ButtonStyle[type_text]}>{text}</Text>
         </TouchableOpacity>
     )
