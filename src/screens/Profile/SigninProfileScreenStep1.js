246a0f64d1ef75bbd95fb4897ad3324bee8accd0
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-28 00:23:09 +0100

    formating file

--- src/screens/Profile/SigninProfileScreenStep1.js
+++ src/screens/Profile/SigninProfileScreenStep1.js
@@ -15,54 +15,70 @@ import * as Verifier from '@utils/Verifier'
 import GlobalStyle from '@styles/GlobalStyle'
 import ProfileStyle from '@styles/ProfileStyle'
 
-
 export default function SigninProfileScreenStep1({ navigation }) {
-    const [ errors, setErrors ] = useState({})
+    const [errors, setErrors] = useState({})
     const { control, handleSubmit } = useForm()
     const { t } = useTranslation()
 
     const { isLoggedIn } = useSelector((state) => state.auth)
 
-    const onSubmit = data => {
+    const onSubmit = (data) => {
         let err = {}
         data = Tools.objFormat(data)
 
         data.email = data.email.trim()
-        if(!data.email.length) err.email = t('errors:form.input.empty')
-        else if(!Verifier.email(data.email)) err.email = t('errors:form.input.email')
+        if (!data.email.length) err.email = t('errors:form.input.empty')
+        else if (!Verifier.email(data.email))
+            err.email = t('errors:form.input.email')
 
         setErrors(err)
-        if(Tools.objSize(err) === 0) {
+        if (Tools.objSize(err) === 0) {
             navigation.navigate('SigninProfileScreenStep2', {
-                email: data.email
+                email: data.email,
             })
         }
     }
 
     useFocusEffect(() => {
-        if(isLoggedIn) navigation.navigate('HomeProfileScreen')
+        if (isLoggedIn) navigation.navigate('HomeProfileScreen')
     })
 
     return (
         <ScrollView contentContainerStyle={ProfileStyle.container}>
-            <AppTitle text={t('signin')} align="center" icon="3lines" />
-            <AppInput control={control} name="email" type="text" label={t('email')} error={errors.email} />
+            <AppTitle text={t('signin')} align='center' icon='3lines' />
+            <AppInput
+                control={control}
+                name='email'
+                type='text'
+                label={t('email')}
+                error={errors.email}
+            />
 
-            <View style={{marginTop: 10}}>
-                <AppButton text={t('signin2')} onPress={handleSubmit(onSubmit)} />
+            <View style={{ marginTop: 10 }}>
+                <AppButton
+                    text={t('signin2')}
+                    onPress={handleSubmit(onSubmit)}
+                />
             </View>
 
             <View style={{ marginVertical: 15 }}>
-                <TextLine text={t('signin_with')} color={GlobalStyle.color.gray} lineColor={GlobalStyle.color.gray} />
+                <TextLine
+                    text={t('signin_with')}
+                    color={GlobalStyle.color.gray}
+                    lineColor={GlobalStyle.color.gray}
+                />
             </View>
 
             <View style={{ marginTop: 10 }}>
-                <AppButton type="google" text={t('signin_google')} />
+                <AppButton type='google' text={t('signin_google')} />
             </View>
 
             <View style={{ marginTop: 40, alignItems: 'center' }}>
                 <Text style={ProfileStyle.bottom_text}>{t('has_account')}</Text>
-                <Link style={ProfileStyle.link} to={{ screen: 'LoginProfileScreen' }}>
+                <Link
+                    style={ProfileStyle.link}
+                    to={{ screen: 'LoginProfileScreen' }}
+                >
                     {t('login2')}
                 </Link>
             </View>
