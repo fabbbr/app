347575899713f66334606f5923d24074e76e285a
Hugo Lahaxe <lahaxeh@gmail.com>
2022-01-28 00:24:09 +0100

    signin profile step2: preparing data

--- src/screens/Profile/SigninProfileScreenStep2.js
+++ src/screens/Profile/SigninProfileScreenStep2.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { ScrollView, View, Text } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useSelector, useDispatch } from 'react-redux'
@@ -15,11 +15,15 @@ import { clearMessage } from '@slices/message'
 import ProfileStyle from '@styles/ProfileStyle'
 
 export default function SigninProfileScreenStep2({ route }) {
-    const [loading, setLoading] = useState(false) // todo display loading
+    const [loading, setLoading] = useState(false)
     const [errors, setErrors] = useState({})
-    const { control, handleSubmit } = useForm()
-    const { t } = useTranslation()
     const { email } = route.params
+    const { control, handleSubmit, setValue } = useForm()
+    const { t } = useTranslation()
+
+    useEffect(() => {
+        setValue('email', email)
+    }, [])
 
     const { isLoggedIn } = useSelector((state) => state.auth)
     const { message } = useSelector((state) => state.message) // todo display message
@@ -34,26 +38,35 @@ export default function SigninProfileScreenStep2({ route }) {
         data = Tools.objFormat(data)
 
         data.email = data.email.trim()
+
         if (!data.email.length) err.email = t('errors:form.input.empty')
-        else if (!Verifier.email(data.email)) t('errors:form.input.email')
+        else if (!Verifier.email(data.email))
+            err.email = t('errors:form.input.email')
+
         if (!data.username) err.username = t('errors:form.input.empty')
+
         if (!data.password.length) err.password = t('errors:form.input.empty')
         if (!data.password2.length) err.password2 = t('errors:form.input.empty')
-        else if (data.password !== data.password2) err.password2 = t('errors:form.input.password')
-        data.country = 'FR'
+        else if (data.password !== data.password2)
+            err.password2 = t('errors:form.input.password')
+
+        data.country = 'FR' // todo add select country
 
         setErrors(err)
         if (Tools.objSize(err) === 0) {
             setLoading(true)
-            
-            try {
-                await dispatch(register({
-                    username: data.username,
-                    email : data.email,
-                    password: data.password
-                }))
 
-                navigation.navigate('HomeProfileScreen')
+            try {
+                const response = await dispatch(
+                    register({
+                        username: data.username,
+                        email: data.email,
+                        password: data.password,
+                        country: data.country,
+                    })
+                )
+
+                if (response) navigation.navigate('HomeProfileScreen')
             } catch {
                 setLoading(false)
             }
@@ -61,20 +74,52 @@ export default function SigninProfileScreenStep2({ route }) {
     }
 
     useFocusEffect(() => {
-        if(isLoggedIn) navigation.navigate('HomeProfileScreen')
+        if (isLoggedIn) navigation.navigate('HomeProfileScreen')
     })
 
     return (
         <ScrollView contentContainerStyle={ProfileStyle.container}>
-            <AppTitle text={t('signin')} align="center" icon="3lines" />
+            <AppTitle text={t('signin')} align='center' icon='3lines' />
 
-            <AppInput control={control} name="email" type="text" label={t('email')} required error={errors.email} defaultValue={email} />
-            <AppInput control={control} name="username" type="text" label={t('username')} required error={errors.username} />
-            <AppInput control={control} name="password" type="password" label={t('password')} required error={errors.password} />
-            <AppInput control={control} name="password2" type="password" label={t('password_confirmation')} required error={errors.password2} />
+            <AppInput
+                control={control}
+                name='email'
+                type='text'
+                label={t('email')}
+                required
+                error={errors.email}
+            />
+            <AppInput
+                control={control}
+                name='username'
+                type='text'
+                label={t('username')}
+                required
+                error={errors.username}
+            />
+            <AppInput
+                control={control}
+                name='password'
+                type='password'
+                label={t('password')}
+                required
+                error={errors.password}
+            />
+            <AppInput
+                control={control}
+                name='password2'
+                type='password'
+                label={t('password_confirmation')}
+                required
+                error={errors.password2}
+            />
 
             <View style={{ marginTop: 10 }}>
-                <AppButton text={t('login')} onPress={handleSubmit(onSubmit)} />
+                <AppButton
+                    text={t('signin2')}
+                    onPress={handleSubmit(onSubmit)}
+                    loading={loading}
+                />
             </View>
         </ScrollView>
     )
