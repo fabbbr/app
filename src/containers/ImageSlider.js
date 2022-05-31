d63aedae3c458e6c7ac6f46ffa47d8ae550da685
Hugo Lahaxe <lahaxeh@gmail.com>
2022-05-31 16:45:42 +0200

    feat: image slider

---
+++ src/containers/ImageSlider.js
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { ScrollView, StyleSheet, View, Image } from 'react-native'
+
+export default function ImageSlider({ images }) {
+    return (
+        <ScrollView
+            horizontal={true}
+            style={styles.container}
+            showsHorizontalScrollIndicator={false}
+        >
+            {images.map((image, index) => {
+                return (
+                    <View style={styles.img_container} key={index}>
+                        <Image source={{ uri: image }} style={styles.img} />
+                    </View>
+                )
+            })}
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 0,
+        marginHorizontal: -5,
+    },
+    img_container: {
+        width: 240,
+        height: 240,
+        backgroundColor: '#EBEBEB',
+        margin: 5,
+    },
+    img: {
+        width: 240,
+        height: 240,
+    },
+})
