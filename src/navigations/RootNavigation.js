06a6691c745d0f32672252fff3c68c1e4e3a293a
Lahaxe Hugo <lahaxeh@gmail.com>
2022-06-03 15:59:42 +0200

    fix: module react navigation error

--- src/navigations/RootNavigation.js
+++ src/navigations/RootNavigation.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useTranslation } from 'react-i18next'
-import { getHeaderTitle } from '@react-navigation/elements'
 
 import HomeScreen from '@screens/root/HomeScreen'
 import ProductScreen from '@screens/root/ProductScreen'
