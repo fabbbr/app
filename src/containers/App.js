60aa5a99cb897d2234ab208aa51363ba2f28b040
Hugo Lahaxe <lahaxeh@gmail.com>
2022-06-04 17:39:17 +0200

    refactor: async app loading

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useDispatch, useSelector } from 'react-redux'
 import Toast from 'react-native-toast-message'
+import * as Message from '@utils/Message'
 
 import Loading from '@containers/Loading'
 import BottomNavigation from '@navigations/BottomNavigation'
@@ -10,26 +11,40 @@ import { setUserInit } from '@slices/auth'
 import { getFonts } from '@constants/fonts'
 
 export default function App() {
-    const [loaded, setLoaded] = useState(false)
+    const [appIsReady, setAppIsReady] = useState(false)
 
     const dispatch = useDispatch()
     const { authInit } = useSelector((state) => state.auth)
 
     useEffect(() => {
         const asyncStart = async () => {
-            const isLoaded =
-                (authInit ? await dispatch(setUserInit()) : true) &&
-                (await getFonts())
-            setLoaded(true)
+            try {
+                if (authInit) await dispatch(setUserInit())
+                await getFonts()
+            } catch (err) {
+                Message.error({ text1: err })
+            } finally {
+                setAppIsReady(true)
+            }
         }
         asyncStart()
     }, [])
 
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            await SplashScreen.hideAsync()
+        }
+    }, [appIsReady])
+
+    if (!appIsReady) {
+        return null
+    }
+
     return (
         <>
-            <Loading data={loaded}>
+            <Loading data={appIsReady} onLayout={onLayoutRootView}>
                 <SafeAreaView style={{ flex: 1 }}>
-                    {loaded ? (
+                    {appIsReady ? (
                         <NavigationContainer>
                             <BottomNavigation />
                         </NavigationContainer>
